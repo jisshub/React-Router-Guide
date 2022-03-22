9e98855dc358d92adcae76300b88855f57b0054d
jisshub <jissmon476.com>
2022-03-22 13:06:34 +0530

    Query Parameters - React router ends

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useLocation } from "react-router-dom";
 
 export default function Contact() {
+  const queryString = useLocation().search;
+  console.log(queryString);
+  const queryParams = new URLSearchParams(queryString);
+  const name = queryParams.get('name');
+
   return (
     <div>
-        <h1>Contact</h1>
+        <h1>Hey {name}, Contact Me</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure officiis natus eligendi in adipisci culpa fugiat distinctio possimus debitis enim reiciendis harum recusandae quaerat accusantium quas, ex qui nulla consectetur.</p>
     </div>
 
